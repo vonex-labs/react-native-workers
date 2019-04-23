490c1fdc25e1bc2996aef4d60209a05aa1e9d9f2
Mihail Gutan <Mihail@gingerlabs.com>
2019-04-23 14:18:48 -0700

    Fix api break

--- android/src/main/java/com/staltz/react/workers/WorkersInstanceManager.java
+++ android/src/main/java/com/staltz/react/workers/WorkersInstanceManager.java
@@ -14,6 +14,7 @@ import com.facebook.react.bridge.WritableMap;
 
 import javax.annotation.Nullable;
 
+@ReactModule(name="WorkersInstanceManager")
 public class WorkersInstanceManager extends ReactContextBaseJavaModule {
 
   private final ReactContext context;
